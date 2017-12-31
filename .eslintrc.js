ecd0b5e3f70742c00c63e4baa3dbd72532f0145c
Freddy Fallon <freddyfallon@gmail.com>
2017-12-31 12:18:06 +0000

    🔧 add eslint.rc

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  env: {
+    es6: "true",
+    browser: "true",
+    node: "true"
+  },
+  extends: "airbnb",
+  rules: {
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+  }
+};
