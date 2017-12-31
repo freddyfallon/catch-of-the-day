bce7823e6b3647e7c5a994fbec4949f0c54c352e
Freddy Fallon <freddyfallon@gmail.com>
2017-12-31 12:17:19 +0000

    🎨 Refactor StorePicker to be stateless component

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,15 +1,13 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
-class StorePicker extends React.Component {
-  render() {
-    return (
-      <form className="store-selector">
-        <h2>Please enter a store</h2>
-        <input type="text" required placeholder="Store name"/>
-        <button type="submit">Visit store</button>
-      </form>
-    )
-  }
-}
+const StorePicker = () =>
+  (
+    <form className="store-selector">
+      <h2>Please enter a store</h2>
+      <input type="text" required placeholder="Store name" defaultValue={getFunName()} />
+      <button type="submit">Visit store</button>
+    </form>
+  );
 
 export default StorePicker;
