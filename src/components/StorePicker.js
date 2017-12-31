2f8eb450caad639651412876b5586fb72c1eeb06
Freddy Fallon <freddyfallon@gmail.com>
2017-12-31 15:00:40 +0000

    ✨ Update Storepicker to capture input

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import { getFunName } from '../helpers';
 
-const StorePicker = () =>
-  (
-    <form className="store-selector">
-      <h2>Please enter a store</h2>
-      <input type="text" required placeholder="Store name" defaultValue={getFunName()} />
-      <button type="submit">Visit store</button>
-    </form>
-  );
+class StorePicker extends React.Component {
+
+  goToStore(event) {
+    event.preventDefault();
+    console.log(this.storeInput.value);
+    console.log('You changed the URL');
+  }
+
+  render() {
+    return (
+      <form className="store-selector" onSubmit={e => this.goToStore(e)}>
+        <h2>Please enter a store</h2>
+        <input
+          type="text"
+          required
+          placeholder="Store name"
+          defaultValue={getFunName()} 
+          ref={(input) => { this.storeInput = input; }}
+        />
+        <button type="submit">Visit store</button>
+      </form>
+    );
+  }
+
+}
 
 export default StorePicker;
