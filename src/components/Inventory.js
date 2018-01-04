ff0a1f7285cf19e042fd8b6c2978421148811ee1
Freddy Fallon <freddyfallon@gmail.com>
2018-01-04 17:17:15 +0000

    ✨ Add addFish, loadSamples props, and PropTypes validation to Inventory

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import AddFishForm from './AddFishForm';
 
-class Inventory extends React.Component {
-  render() {
-    return (
-      <p>Inventory</p>
-    )
-  }
-}
+const Inventory = props => (
+  <div>
+    <h2>Inventory</h2>
+    <AddFishForm addFish={props.addFish} />
+    <button onClick={props.loadSamples}>Load Sample Fishes</button>
+  </div>
+);
+
+Inventory.propTypes = {
+  addFish: PropTypes.func.isRequired,
+  loadSamples: PropTypes.func.isRequired,
+};
 
 export default Inventory;
