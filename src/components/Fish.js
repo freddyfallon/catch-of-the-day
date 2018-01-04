f817b9a896ff1197ce42b91d679a89d316e82a7a
Freddy Fallon <freddyfallon@gmail.com>
2018-01-04 17:37:38 +0000

    ✨ Add fish details to Fish component JSX

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -1,8 +1,17 @@
 import React, { PropTypes } from 'react';
+import { formatPrice } from '../helpers';
 
-const Fish = props => (
-  <li className="menu-fish">{props.details.name}</li>
-);
+const Fish = (props) => {
+  const { details } = props;
+  return (
+    <li className="menu-fish">
+      <img src={details.image} alt={details.name} />
+      <h3 className="fish-name">{details.name}</h3>
+      <span className="price">{ formatPrice(details.price)}</span>
+      <p>{details.desc}</p>
+    </li>
+  );
+};
 
 Fish.propTypes = {
   details: PropTypes.shape({
