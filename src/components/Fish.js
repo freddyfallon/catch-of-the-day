12b86e20aff6d224ec00fec780b0483e13677840
Freddy Fallon <freddyfallon@gmail.com>
2018-01-11 08:30:08 +0000

    ✨ Add order button to Fish

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -2,13 +2,16 @@ import React, { PropTypes } from 'react';
 import { formatPrice } from '../helpers';
 
 const Fish = (props) => {
-  const { details } = props;
+  const { details, index } = props;
+  const isAvailable = details.status === 'available';
+  const buttonText = isAvailable ? 'Add To Order' : 'Sold Out';
   return (
     <li className="menu-fish">
       <img src={details.image} alt={details.name} />
       <h3 className="fish-name">{details.name}</h3>
       <span className="price">{ formatPrice(details.price)}</span>
       <p>{details.desc}</p>
+      <button onClick={() => props.addToOrder(index)} disabled={!isAvailable}>{buttonText}</button>
     </li>
   );
 };
@@ -21,5 +24,7 @@ Fish.propTypes = {
     desc: PropTypes.string,
     image: PropTypes.string,
   }).isRequired,
+  addToOrder: PropTypes.func.isRequired,
+  index: PropTypes.string.isRequired,
 };
 export default Fish;
